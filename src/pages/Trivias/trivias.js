6dbd29c42ab2460ff6b34703f418017a571e5d1d
Lily-j17-source <liliana.1793@hotmail.com>
2021-04-24 18:30:36 -0500

    Fix pages pce stages: DiagnosisDescription, EvaluationDescription, Execution and Planning Description, Add icon for pages, and Add information for Trivias

--- src/pages/Trivias/trivias.js
+++ src/pages/Trivias/trivias.js
@@ -29,10 +29,10 @@ export default class TriviasPage extends Component {
             <Card className="d-flex flex-row card_trivia card_trivia_pce  col-12 col-md-6 ">
               <Card.Img src={PCEImg} className=" card_trivia_img"/>
               <Card.Body>
-                <Card.Title className="card_trivia_title">Etapas del Proceso Cuidado Enfermero</Card.Title>
+                <Card.Title className="card_trivia_title">Tema: Cefalocaudal</Card.Title>
                 <Card.Text className="card_trivia_text">
-                  Some quick example text to build on the card title and make up the bulk of
-                  the card's content.
+                  Proviene del metodo de Observación que es un proceso logico y deliberado para obtener informacion
+                  a través de los órganos de los sentidos.
                 </Card.Text>
                 <Card.Text className="card_trivia_text d-flex justify-content-between align-items-center">
                   <div>
@@ -47,10 +47,12 @@ export default class TriviasPage extends Component {
             <Card className="d-flex flex-row card_trivia card_trivia_valoration col-12 col-md-6">
               <Card.Img src={ValorationImg} className=" card_trivia_img"/>
               <Card.Body>
-                <Card.Title className="card_trivia_title">Fases de etapa de Valoración</Card.Title>
+                <Card.Title className="card_trivia_title">Tema: Habitos externos</Card.Title>
                 <Card.Text className="card_trivia_text">
-                  Some quick example text to build on the card title and make up the bulk of
-                  the card's content.
+                Proviene del metodo de Observación.
+                Es el conjunto de datos obtenidos de la inspección general es decir a simple vista 
+                sin realizar ninguna otra maniobra de exploración física y sin ningún interrogatorio. 
+
                 </Card.Text>
                 <Card.Text className="card_trivia_text d-flex justify-content-between align-items-center">
                   <div>
@@ -69,10 +71,10 @@ export default class TriviasPage extends Component {
             <Card className="d-flex flex-row card_trivia card_trivia_data">
               <Card.Img src={DataImg} className=" card_trivia_img"/>
               <Card.Body>
-                <Card.Title className="card_trivia_title">Métodos de obtención de datos</Card.Title>
+                <Card.Title className="card_trivia_title">Tema: Patrones funcionales</Card.Title>
                 <Card.Text className="card_trivia_text">
-                  Some quick example text to build on the card title and make up the bulk of
-                  the card's content.
+                  El método de valoración por patrones funcionales de salud fue desarrollado Marjory Gordon y
+                  proporcionan una estructura lógica de valoración y una base de datos para el diagnóstico enfermero.
                 </Card.Text>
                 <Card.Text className="card_trivia_text d-flex justify-content-between align-items-center">
                   <div>
@@ -87,10 +89,10 @@ export default class TriviasPage extends Component {
             <Card className="d-flex flex-row card_trivia card_trivia_observation">
               <Card.Img src={ObservationImg} className=" card_trivia_img"/>
               <Card.Body>
-                <Card.Title className="card_trivia_title">Observación del paciente</Card.Title>
+                <Card.Title className="card_trivia_title">Tema: Anamnesis de enfermeria</Card.Title>
                 <Card.Text className="card_trivia_text">
-                  Some quick example text to build on the card title and make up the bulk of
-                  the card's content.
+                  Proviene del metodo de entrevista, es una conversación planificada para 
+                  dar y obtener información e identificar problemas para identificar personalmente al individuo.
                 </Card.Text>
                 <Card.Text className="card_trivia_text d-flex justify-content-between align-items-center">
                   <div>
@@ -107,10 +109,10 @@ export default class TriviasPage extends Component {
             <Card className="d-flex flex-row card_trivia card_trivia_interview">
               <Card.Img src={InterviewImg} className=" card_trivia_img"/>
               <Card.Body>
-                <Card.Title className="card_trivia_title">Entrevista al paciente</Card.Title>
+                <Card.Title className="card_trivia_title">Tema: Palpación</Card.Title>
                 <Card.Text className="card_trivia_text">
-                  Some quick example text to build on the card title and make up the bulk of
-                  the card's content.
+                  Proviene del metodo Exploración física, se practica a toda persona a fin de 
+                  reconocer las alteraciones o signos producidos por la enfermedad.
                 </Card.Text>
                 <Card.Text className="card_trivia_text d-flex justify-content-between align-items-center">
                   <div>
@@ -125,10 +127,9 @@ export default class TriviasPage extends Component {
             <Card className="d-flex flex-row card_trivia card_trivia_exploration">
               <Card.Img src={ExplorationImg} className=" card_trivia_img"/>
               <Card.Body>
-                <Card.Title className="card_trivia_title">Exploración física del paciente</Card.Title>
+                <Card.Title className="card_trivia_title">Tema: Inspección</Card.Title>
                 <Card.Text className="card_trivia_text">
-                  Some quick example text to build on the card title and make up the bulk of
-                  the card's content.
+                Proviene del metodo Exploración física y tiene como objetivo: confirmar los datos obtenidos, detectar nuevos signos o síntomas.
                 </Card.Text>
                 <Card.Text className="card_trivia_text d-flex justify-content-between align-items-center">
                   <div>
