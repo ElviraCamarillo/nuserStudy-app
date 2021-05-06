5c96d3a44206bf57537014c868a157c83e8ef354
Gabriela Elvira Camarillo <gcamarilloluna@gmail.com>
2021-05-05 22:23:24 -0500

    MOD style question:octocat:

--- src/components/Questions/questionTypeTwo.js
+++ src/components/Questions/questionTypeTwo.js
@@ -8,53 +8,49 @@ import './Question.css'
 
 
 export default class QuestionTypeTwo extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            data: this.props
-        }
-    }
-    handleQuestionSelect = (event) => {
-        console.log(event)
-        console.log(event.target.dataset.res)  
-        let responseSelect = event.target.dataset.res
-        this.props.onSelectQuestion(responseSelect)
+  constructor(props){
+      super(props);
+      this.state = {
+          data: this.props
+      }
+  }
+  handleQuestionSelect = (event) => {
+      console.log(event)
+      console.log(event.target.dataset.res)  
+      let responseSelect = event.target.dataset.res
+      this.props.onSelectQuestion(responseSelect)
 
-    }
+  }
 
-    render(){
-        
-        console.log(this.state)
-        const answers = this.state.data.data
-        const correct = this.state.data.correct
-        const question = this.state.data.question
-        const filteredData = answers.filter(function (el) {
-            return el != "";
-        })
-        
-        return(
-            <div>
-
-                <div className="container">
-                    <h1 className="mt-5 mb-3 title__trivia">{question} :</h1>
-                </div>
-
-                <div className="mt-5 mb-5 grid__answers">
-                    <div className="row">
-                        {
-                            filteredData.map((option)=> {
-                                return  (
-                                    <div className="col-12 col-md-6 mb-4 d-flex justify-content-center">
-                                        <button class="btn btn__answer" data-res={option} onClick={this.handleQuestionSelect}>{option}</button>
-                                    </div>
-                                )
-                            })
-                        }
-                        
+  render(){
+    
+    console.log(this.state)
+    const answers = this.state.data.data
+    const correct = this.state.data.correct
+    const question = this.state.data.question
+    const filteredData = answers.filter(function (el) {
+        return el != "";
+    })
+    
+    return(
+      <div>
+        <div className="container">
+            <h1 className="mt-5 mb-3 title__trivia">{question}</h1>
+        </div>
+        <div className="mt-5 mb-5 grid__answers">
+            <div className="row">
+              {
+                filteredData.map((option)=> {
+                  return  (
+                    <div className="col-12 col-md-6 mb-4 d-flex justify-content-center">
+                      <button class="btn btn__answer" data-res={option} onClick={this.handleQuestionSelect}>{option}</button>
                     </div>
-                </div>
-
+                  )
+                })
+              }
             </div>
-        )
-    }
+        </div>
+      </div>
+    )
+  }
 }
